44fbc074913e978a3478432fba707eb8849f8d33
Ashwin mk <121005335+ashwin-05-22@users.noreply.github.com>
2022-12-21 10:41:59 +0530

    hi

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/material.dart';
+import 'package:flutter/material.dart'; 
 
 void main() => runApp(Quizzler());
 
@@ -32,7 +32,7 @@ class _QuizPageState extends State<QuizPage> {
     'A slug\'s blood is green.?',
   ];
 
-  List <bool> answers = [false,true,true];
+  List<bool> answers = [false, true, true];
 
   int questionNumber = 0;
 
@@ -76,23 +76,18 @@ class _QuizPageState extends State<QuizPage> {
 
                 bool correctAnswer = answers[questionNumber];
 
-                  if(correctAnswer == true){
-
+                if (correctAnswer == true) {
                   print('user is right');
-                }else{
+                } else {
                   print('user wrong');
+                }
 
-            
                 setState(() {
                   questionNumber++;
                 });
-                }
 
-
-              
                 print(questionNumber);
               },
-              
             ),
           ),
         ),
@@ -112,10 +107,10 @@ class _QuizPageState extends State<QuizPage> {
               onPressed: () {
                 //The user picked false.
 
-                 bool correctAnswer = answers[questionNumber];
-                if(correctAnswer == false){
+                bool correctAnswer = answers[questionNumber];
+                if (correctAnswer == false) {
                   print('user is right');
-                }else{
+                } else {
                   print('user wrong');
                 }
                 setState(() {
